06437c3a2ad5591ad6b053282260113082e030fb
AnwarBari <33730047+AnwarBari@users.noreply.github.com>
2018-10-04 09:57:14 -0400

    Your commit message

---
+++ app/src/main/java/com/example/sofe4640/xmlparser/XmlParser.java
@@ -0,0 +1,18 @@
+package com.example.sofe4640.xmlparser;
+
+import android.content.res.XmlResourceParser;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class XmlParser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xml_parser);
+
+
+
+    }
+
+}
